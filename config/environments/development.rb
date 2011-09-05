2c5ec81b450ab9a9561ba6401b90a0bbe467835d
Jim Knight <jim.knight@lavatech.com>
2011-09-05 14:59:20 -0400

    added slim gem and converted application

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,8 @@ University::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # Slim
+  Slim::Engine.set_default_options :pretty => true
+  
 end
