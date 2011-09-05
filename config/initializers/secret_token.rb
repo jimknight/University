0176bde4d720efa5d0c1f1c3dd69466e80ffa9a9
Jim Knight <jim.knight@lavatech.com>
2011-09-05 13:53:27 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+University::Application.config.secret_token = 'fab26e800883205393dfaf2ba5abd6b755f60e6da3885a31f0d58b2989e1c7f855248adb86f0f68fbb16f778432d36d3208512301bfcd4f5b0bc760968d40f9d'
